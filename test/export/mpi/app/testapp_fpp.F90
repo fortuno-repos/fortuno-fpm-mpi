ecf91e14cc25c5a6375906f47dcafa1e2d409fc9
fortuno-repos <aradi@uni-bremen.de>
2024-10-03 18:20:04 +0000

    Update from https://github.com/fortuno-repos/fortuno/commit/7d9684cc3596053d437a1a8d2d913bc1e05fe288

--- test/export/mpi/app/testapp_fpp.F90
+++ test/export/mpi/app/testapp_fpp.F90
@@ -2,47 +2,44 @@
 ! Licensed under the BSD-2-Clause Plus Patent license.
 ! SPDX-License-Identifier: BSD-2-Clause-Patent
 
-#:include "fortuno_mpi.fypp"
+#include "fortuno_mpi.fpp"
 
 !> Unit tests
-module testapp_mpi_fypp_tests
+module testapp_mpi_fpp_tests
   use mpi_f08, only : mpi_bcast, MPI_INTEGER
-  use fortuno_mpi, only : global_comm, is_equal, test_list, this_rank
-  $:FORTUNO_MPI_IMPORTS()
+  use fortuno_mpi, only : global_comm, is_equal, test => mpi_case_item, test_list, this_rank
   implicit none
 
 contains
 
-
-  $:TEST("success")
+  subroutine test_success()
 
     integer :: buffer
 
     buffer = 0
     if (this_rank() == 0) buffer = 1
     call mpi_bcast(buffer, 1, MPI_INTEGER, 0, global_comm())
-    @:CHECK(is_equal(buffer, 1))
+    CHECK(is_equal(buffer, 1))
 
-  $:END_TEST()
+  end subroutine test_success
 
 
   function tests()
     type(test_list) :: tests
 
     tests = test_list([&
-        $:TEST_ITEMS()
+        test("success", test_success)&
     ])
-    $:STOP_ON_MISSING_TEST_ITEMS()
 
   end function tests
 
-end module testapp_mpi_fypp_tests
+end module testapp_mpi_fpp_tests
 
 
 !> Test app driving Fortuno unit tests
 program testapp
   use fortuno_mpi, only : execute_mpi_cmd_app
-  use testapp_mpi_fypp_tests, only : tests
+  use testapp_mpi_fpp_tests, only : tests
   implicit none
 
   call execute_mpi_cmd_app(tests())
