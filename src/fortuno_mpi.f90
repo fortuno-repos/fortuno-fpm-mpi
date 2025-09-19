12b7e6ab260a8acb5b754ca5abe84b8a7b6d6d3b
fortuno-repos <aradi@uni-bremen.de>
2025-09-19 16:48:27 +0000

    Update from https://github.com/fortuno-repos/fortuno/commit/7dc5d19f9ee1cbb5e38cb40accbf4ac165c2b058

--- src/fortuno_mpi.f90
+++ src/fortuno_mpi.f90
@@ -5,10 +5,11 @@
 !> Exports the MPI-dependent part of Fortuno
 module fortuno_mpi
   use fortuno
+  use fortuno_mpi_mpibasetypes, only : mpi_case_base, mpi_suite_base
   use fortuno_mpi_mpicmdapp, only : mpi_cmd_app, execute_mpi_cmd_app
   use fortuno_mpi_mpicontext, only : mpi_context
   use fortuno_mpi_mpiglobalctx, only : global_comm, global_comm_id, mpi_check, mpi_check_failed,&
-      & mpi_failed, mpi_skip, mpi_scope_pointers, num_ranks, this_rank
+      & mpi_failed, mpi_skip, mpi_skipped, mpi_scope_pointers, num_ranks, this_rank
   use fortuno_mpi_mpicase, only : mpi_case, mpi_case_item
   use fortuno_mpi_mpisuite, only : mpi_suite, mpi_suite_item
   implicit none
